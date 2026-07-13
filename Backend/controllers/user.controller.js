259cf4cc088a2525d5a7ee0640f984653e2fe247
mitalis435 <mitalis423@gmail.com>
2026-07-13 22:23:06 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -127,7 +127,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "Strict",
+        sameSite: "none",
+        secure: true
       })
       .json({
         message: `Welcome back ${user.fullname}`,
