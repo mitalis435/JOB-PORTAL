1de0c91b3bdf3ca0934ffa9e75468137b08cb939
mitalis435 <mitalis423@gmail.com>
2026-07-13 21:00:34 +0530

    Update API URL and gitignore

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "${process.env.REACT_APP_API_URL}/api/v1/user";
-export const COMPANY_API_ENDPOINT = "${process.env.REACT_APP_API_URL}/api/v1/company";
-export const JOB_API_ENDPOINT = "${process.env.REACT_APP_API_URL}/api/v1/job";
-export const APPLICATION_API_ENDPOINT = "${process.env.REACT_APP_API_URL}/api/v1/application";
+export const USER_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/user`;
+export const COMPANY_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/company`;
+export const JOB_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/job`;
+export const APPLICATION_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/application`;
