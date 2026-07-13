de5c358474feb4084326ab0514106bb97558d98b
mitalis435 <mitalis423@gmail.com>
2026-07-13 21:07:17 +0530

    Update data.js

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,6 @@
-export const USER_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/user`;
-export const COMPANY_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/company`;
-export const JOB_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/job`;
-export const APPLICATION_API_ENDPOINT = `${process.env.REACT_APP_API_URL}/api/v1/application`;
+const BASE_URL = "https://job-portal-wihn.onrender.com/api/v1";
+
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
